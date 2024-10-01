40ce6a491ad10bf427a149afb27fb5ba61b07c51
Marta Jaekel <martajaekel@gmail.com>
2024-10-01 13:26:19 +0200

    fix build error

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
 import Image from "next/image";
-import media from "css-in-js-media";
 
 export default function About() {
   return (
