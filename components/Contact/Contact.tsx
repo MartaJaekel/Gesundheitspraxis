f9e6bcf4061ffb35acb93fa954d1e6bb7f50aac5
Marta Jaekel <martajaekel@gmail.com>
2026-09-24 17:18:09 +0200

    take out address mitte

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -128,10 +128,7 @@ export default function Contact() {
                   <img src="/location.png" alt="location" />
                   <p>Weichselplatz 4 - 12045 Berlin</p>
                 </IconWrapper>
-                 <IconWrapper>
-                  <img src="/location.png" alt="location" />
-                  <p> Brunnenstraße 147 - 10115 Berlin </p>
-                </IconWrapper>
+                
               </StyledDetails>
             </Info>
           </StyledSection>
